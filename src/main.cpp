7fd0288a70f5a602e57f50355c2e9893fe3e6e6d
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-06-13 02:50:46 -0400

    delete whack stuff from main and make skelly

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "sensor.hpp"
+#include "pump.hpp"
+
+// Initialize devices
+// Sensor sensor_A(FAKE_GPIO_1);
+// Pump pump_A(FAKE_GPIO_2);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // PASS
+}
