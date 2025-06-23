cf1b1becabe3f5f60e50250502d4899d39a6c993
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-06-23 03:58:27 -0400

    Add sensor measurement task

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,61 @@
 #include "sensor.hpp"
 #include "pump.hpp"
 
-#define GPIO_A 32
-#define GPIO_B 16
+#define GPIO_SENSOR 32
+#define GPIO_PUMP 16
+
+struct PlantData {
+  int moisture;
+};
 
 const int DRY_THRESHOLD = 3000;
 
+// Initialize data
+PlantData plantData;
+
 // Initialize devices
-Sensor sensor(GPIO_A);
-Pump pump(GPIO_B);
+Sensor sensor(GPIO_SENSOR);
+Pump pump(GPIO_PUMP);
+
+void SensorTask(void* pvParameters) {
+  for (;;) {
+    plantData.moisture = sensor.readMoisture();
+
+    // Testing
+    Serial.print("Moisture: ");
+    Serial.println(plantData.moisture);
+
+    vTaskDelay( pdMS_TO_TICKS(1000) );
+  }
+  
+}
 
 void setup() {
   Serial.begin(115200);
+
+  xTaskCreate(
+    SensorTask, 
+    "Sensor", 
+    1024, 
+    NULL, 
+    1, 
+    NULL
+  );
 }
 
 void loop() {
   // Test sensor
-  int moisture = sensor.readMoisture();
-  Serial.print("Moisture: ");
-  Serial.println(moisture);
+  // int moisture = sensor.readMoisture();
+  // Serial.print("Moisture: ");
+  // Serial.println(moisture);
 
   // Test pump toggling
-  if (moisture < DRY_THRESHOLD) {
-    pump.setState(true);
-  }
-  else {
-    pump.setState(false);
-  }
+  // if (moisture < DRY_THRESHOLD) {
+  //   pump.setState(true);
+  // }
+  // else {
+  //   pump.setState(false);
+  // }
   
-  delay(1000);
+  // delay(1000);
 }
