2b37b9c88d664e62e12d206eb91f7f15a1a031ec
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-06-30 22:49:10 -0400

    Convert raw sensor readings to %

--- inc/config.hpp
+++ inc/config.hpp
@@ -9,7 +9,7 @@ namespace Config {
     constexpr TickType_t SENSOR_TASK_INTERVAL  = pdMS_TO_TICKS(1000);
     constexpr TickType_t CONTROL_TASK_INTERVAL = pdMS_TO_TICKS(500);
 
-    constexpr unsigned long WATERING_DURATION_MS = 3500;    // Time needed to pump ~100 mL of water
+    constexpr unsigned long WATERING_DURATION_MS = 3500;    // Time to pump ~100 mL of water (x1 per week frequency)
     constexpr unsigned long COOLDOWN_DURATION_MS = 5000;
 
     constexpr unsigned long WIFI_RECONNECT_INTERVAL_MS = 5000;
@@ -21,9 +21,19 @@ namespace Config {
     constexpr const char* MQTT_TOPIC     = "quenchtessential/plant/status";
     constexpr int         MQTT_PORT      = 1883;
 
-    constexpr int DRY_THRESHOLD = 3200;     // No water for 1 week
-    constexpr int WET_THRESHOLD = 2775;     // Watered with 1 cup
+    // Used to arbitrarily set absolute thresholds
+    constexpr int PLANT_DRY_THRESHOLD_RAW = 3200;     // No water for 1 week
+    constexpr int PLANT_WET_THRESHOLD_RAW = 2775;     // Watered with 1 cup
 
-    constexpr int MIN_RAW_MOISTURE = 1650;  // Completely submerged in water
-    constexpr int MAX_RAW_MOISTURE = 3450;  // Sitting dry in air
+    // Arbitrary thresholds
+    constexpr int PLANT_ABSOLUTE_DRY_THRESHOLD_RAW = 3250;     // "Lower" bound for percentage
+    constexpr int PLANT_ABSOLUTE_WET_THRESHOLD_RAW = 2500;     // "Upper" bound for percentage
+
+    // Actual raw moisture value used when determining when to water
+    constexpr int PLANT_WATERING_THRESHOLD_RAW = 3000;      // Aiming for watering twice a week with half the weekly dose (100 mL every 2 days)
+    constexpr int PLANT_WATERING_THRESHOLD_PERCENT = 33;    // = [ 100 * (3250 - 3000) ] / (3250 - 2500) 
+
+    // Mostly for reference
+    constexpr int SENSOR_DRY_RAW = 1650;  // Sensor completely submerged in water (minimum)
+    constexpr int SENSOR_WET_RAW = 3450;  // Sensor sitting dry in air (maximum)
 }
--- src/sensor.cpp
+++ src/sensor.cpp
@@ -1,10 +1,15 @@
 #include "sensor.hpp"
+
 #include <Arduino.h>
+#include <config.hpp>
 
 Sensor::Sensor(int pin) : pin_(pin) {
     pinMode(pin_, INPUT);
 }
 
 int Sensor::readMoisture() {
-    return analogRead(pin_);
+    int raw = analogRead(pin_);
+    int percent = map(raw, PLANT_DRY_THRESHOLD_RAW, PLANT_WET_THRESHOLD_RAW, 0, 100);
+
+    return constrain(percent, 0, 100);
 }
